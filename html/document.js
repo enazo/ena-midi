e3db80e8aebf853f0304477595497daf4fc6043d
i7 <592668024@qq.com>
2024-02-12 11:46:04 +0800

    loop filter 加入按钮

--- html/document.js
+++ html/document.js
@@ -3,42 +3,50 @@
 const tunes = [
 	{
 		"name": "do",
-		"key": "1",
+		key: "1",
+		button: "1",
 		"filename": "200-do"
 	},
 	{
 		"name": "re",
-		"key": "2",
+		key: "2",
+		button: "2",
 		"filename": "201-re"
 	},
 	{
 		"name": "mi",
-		"key": "3",
+		key: "3",
+		button: "3",
 		"filename": "202-mi"
 	},
 	{
 		"name": "fa",
-		"key": "4",
+		key: "4",
+		button: "4",
 		"filename": "203-fa"
 	},
 	{
 		"name": "so",
-		"key": "5",
+		key: "5",
+		button: "5",
 		"filename": "204-so"
 	},
 	{
 		"name": "ra",
-		"key": "6",
+		key: "6",
+		button: "6",
 		"filename": "205-ra"
 	},
 	{
 		"name": "shi",
-		"key": "7",
+		key: "7",
+		button: "7",
 		"filename": "206-shi"
 	},
 	{
 		"name": "do",
-		"key": "8",
+		key: "8",
+		button: "8",
 		"filename": "207-do"
 	}
 ];
--- html/tune.js
+++ html/tune.js
@@ -23,7 +23,7 @@ class Tune {
 		this.canvas = document.createElement('canvas');
 		this.canvas.width = appWidth;
 		this.canvas.height = appHeight;
-		appEl.querySelector('.tunes').appendChild(this.canvas);
+		appEl.appendChild(this.canvas);
 		this.ctx = this.canvas.getContext('2d');
 		this.ctx.fillStyle = 'black';
 
@@ -38,6 +38,8 @@ class Tune {
 
 		this.tunes = tunes;
 		this.keyToTune = {};
+		this.buttons = [];
+
 
 
 		window.addEventListener('keydown',e=>this.onKeyDown(e));
@@ -53,6 +55,9 @@ class Tune {
 			tune.sources = [];
 
 			this.keyToTune[tune.key] = tune;
+
+			tune.type = 'tune';
+			this.buttons.push(tune);
 			
 			fetch(this.filenameToURL(filename))
 				.then(response => response.arrayBuffer())
@@ -62,6 +67,31 @@ class Tune {
 				});
 		});
 
+
+
+
+
+		this.loop = false;
+		this.buttons[9] = {
+			type: 'func',
+			text: 'Loop',
+			onClick: () => {
+				this.loop = !this.loop;
+				this.drawCanvas();
+			}
+		}
+
+		this.filter = false;
+		this.buttons[10] = {
+			type: 'func',
+			text: 'Filter',
+			onClick: () => {
+				this.filter = !this.filter;
+				this.drawCanvas();
+			}
+		}
+
+		
 		this.drawCanvas();
 	}
 
@@ -71,31 +101,51 @@ class Tune {
 		
 		// 绘制 Tune 九宫格
 		const { tuneWidth } = this;
-		this.tunes.forEach( (tune, index) => {
+		this.buttons.forEach( (button, index) => {
 			const x = (index % 3) * tuneWidth;
 			const y = Math.floor(index/3) * tuneWidth;
 
+			let text = '';
+			let active = false;
+			let background = 'rgba(0, 0, 0, 0.1)';
+
+			if(button.type === 'func'){
+				text = button.text;
+				if(button.text === 'Loop'){
+					active = this.loop;
+				}else if(button.text === 'Filter'){
+					active = this.filter;
+				}
+			}else if(button.type === 'tune'){
+
+				const tune = button;
+				text = tune.name;
+				active = tune.sources.length > 0;
+				background = `hsl(${index * 50}deg, 100%, 90%)`;
+				if(active){
+					const lastSource = tune.sources[tune.sources.length - 1];
+					// 获取当前播放源 播放进度
+					const { currentTime } = this.audioContext;
+					const duration = lastSource.buffer.duration;
+	
+	
+					// todo 这里还要记录下当前 source 开始播放时间，减去当前时间，算出当前播放进度
+					console.log('duration', lastSource.buffer.duration);
+	
+				}
+	
+			}
 
 
-			const active = tune.sources.length > 0;
 			if(active){
-
-				const lastSource = tune.sources[tune.sources.length - 1];
-				// 获取当前播放源 播放进度
-				const { currentTime } = this.audioContext;
-				const duration = lastSource.buffer.duration;
-
-
-				// todo 这里还要记录下当前 source 开始播放时间，减去当前时间，算出当前播放进度
-				console.log('duration', lastSource.buffer.duration);
-
-				this.ctx.fillStyle = 'rgba(0, 0, 0, 0.1)';
+				this.ctx.fillStyle = background;
 				this.ctx.fillRect(x, y, tuneWidth, tuneWidth);
 			}
 
+
 			this.ctx.fillStyle = 'black';
 			this.ctx.fillText(
-				tune.name, 
+				text, 
 				x + tuneWidth / 2, 
 				y + tuneWidth / 2, 
 			);
@@ -169,7 +219,7 @@ class Tune {
 
 		tune.lastPlayed = now;
 
-		this.playSound(tune, loopSwitch.checked, filterSwitch.checked);
+		this.playSound(tune, this.loop, this.filter);
 	}
 
 	onKeyDown(e){
@@ -199,11 +249,15 @@ class Tune {
 			Math.floor(x / appNarWidth) + 
 			Math.floor(y / appNarWidth) * 3;
 
-		const tune = this.tunes[index];
 
-		if(!tune) return;
+		const button = this.buttons[index];
+		if(!button) return;
 
-		this.playTune(tune);
+		if(button.type === 'func'){
+			button.onClick();
+		}else if(button.type === 'tune'){
+			this.playTune(button);
+		}
 	}
 }
 
